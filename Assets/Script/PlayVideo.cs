510b1028b0e804b3b1678be2916eea74f2ddf7e7
Somjot <somjot.pro@gmail.com>
2018-12-04 12:24:26 +0700

    first commit

---
+++ Assets/Script/PlayVideo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayVideo : MonoBehaviour {
+	public MovieTexture movTexture;
+	// Use this for initialization
+	void Start () {
+		GetComponent<Renderer> ().material.mainTexture = movTexture;
+		movTexture.Play ();
+		movTexture.loop = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Script/Trans.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trans : MonoBehaviour {
+	public int speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (0, Time.deltaTime * speed, 0);
+	}
+}
